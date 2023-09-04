024e98d5417e483b31e8e6145022e1d6cc73a5b5
pd664 <prateek.dixit664@gmail.com>
2023-09-04 15:55:07 +0530

    make it better

--- src/comps/body/landing/Landing.js
+++ src/comps/body/landing/Landing.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import '../../../static/comps/body/landing.css'
 import ProfilePic from '../../../pictures/2x.jpg'
-
+import Typewriter from 'typewriter-effect';
+import { FiDownload } from 'react-icons/fi'
+import resume from '../../../resume/prateek_resume.pdf'
 function Landing() {
+
   useEffect(() => {
     let i = 0;
     let txt = `<Prateek Dixit />`
@@ -24,21 +27,28 @@ function Landing() {
   }, [])
 
   return (
-    <div className='landing mt-5'>
+    <div className='landing'>
       <div className='landing_cont d-flex justify-content-between flex-wrap'>
-        <div className='landing_left p-2 d-flex flex-column justify-content-strech'>
+        <div className='landing_left my-5 d-flex flex-column justify-content-strech'>
 
-          <div className='landing_title p-1'>
-            <span>Hello, This is </span>
-            <span className='color_yellow' id='color_yellow'></span><br />
+          <div className='landing_title p-1 text-wrap'>
+            <span className=''>Hello, This is </span>
+            <span className='color_yellow_name' id='color_yellow'></span><br />
           </div>
           <div className="h-text">
-            <div className="h-animate">
-              <span className="text first-text">I'm a&nbsp;</span>
-              <span className="text sec-text">Developer</span>
+            <div className="h-animate text-nowrap d-flex">
+              <span className="text first-text text-nowrap d-inline">I'm a &nbsp;</span> <Typewriter
+                options={{
+                  text: 'm a',
+                  strings: ['Developer', 'Freelancer', 'Designer'],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
             </div>
+
           </div>
-          <a href="#contact"><button className='landing_contact p-3'>Contact Me</button></a>
+          <a href={resume} download="Prateek_Resume"><button className='landing_contact p-3'>Download Resume <FiDownload /></button></a>
         </div>
         <div className='landing_right'>
           <img src={ProfilePic} className='profile_pic img-fluid' />
